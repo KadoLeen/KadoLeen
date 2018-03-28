f0dbb4bdb4623b1d121f340295a67cff6b8a9e78
khousheish <khousheish@live.com>
2018-03-28 19:06:27 +0300

    Add blank about and contact pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    KadoLeen
                 </div>
 
                 <div class="links">
