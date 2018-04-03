87b0b6c433ce93e88e1f9131f93ce159fdc85ad4
khousheish <khousheish@live.com>
2018-04-03 21:40:31 +0300

    Fix indentation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,111 +1,75 @@
 <!doctype html>
 <html lang="en">
-        <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="icon" type="image/ico" sizes="16x16" href="{{URL::asset('favicon.ico')}}">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="icon" type="image/ico" sizes="16x16" href="{{URL::asset('favicon.ico')}}">
 
-        <title>KadoLeen</title>
-    </head>
-    <body>
-            {{-- <nav class="navbar fixed-top navbar-expand-lg navbar-light" style="background-color: #cc00ff;">
-                    <a class="navbar-brand" href="#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                      <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                              </form>
-                      </ul>
-                      <ul class="navbar-nav">
-                            <li class="nav-item">
-                                    <a class="nav-link" href="#">Contact Us</a>
-                                  </li>
-                                  <li class="nav-item">
-                                        <a class="nav-link" href="#">About</a>
-                                      </li>
-                      <li class="nav-item active dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                              <a class="dropdown-item" href="#">Action</a>
-                              <a class="dropdown-item" href="#">Another action</a>
-                              <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                          </li>
-                        </li>
-                      </ul>
-                    </div>
-                  </nav> --}}
-                  <nav class="navbar navbar-expand-md navbar-fixed-top navbar-light" style="background-color: #cc00ff;">
-                            <div class="navbar-collapse collapse nav-content order-2 order-md-4">
-
-                                <ul class="nav navbar-nav">
-                                    <li>
-                                        <a href="#" class="pull-left"><img src="favicon.ico"></a>
-                                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                          <span class="navbar-toggler-icon"></span>
-                                        </button>
-                                    </li>
-                                    <li class="nav-item active">
-                                            <a class="nav-link pl-0" href="/">adoLeen</a>
-                                        </li>
-                                    <li class="nav-item active">
-                                        <a class="nav-link" href="/">Home</a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <ul class="nav navbar-nav text-nowrap flex-row mx-md-auto order-1 order-md-4">
-                                    <form class="form-inline my-2 my-lg-0">
-                                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                                    </form>
-                            </ul>
-                            <div class="ml-auto navbar-collapse collapse nav-content order-3 order-md-4">
-                                <ul class="ml-auto nav navbar-nav">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/contact">Contact</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="/about">About</a>
-                                    </li>
-                                    <li class="nav-item active dropdown">
-                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My Account</a>
-                                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                            <a class="dropdown-item" href="#">Profile</a>
-                                            <a class="dropdown-item" href="#">Cart</a>
-                                            <a class="dropdown-item" href="#">Logout</a>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                    </nav>
-                    <div class="container">
-                        <div class="jumbotron jumbotron-fluid">
-                            <div class="container">
-                                <h1 class="display-4">Fluid jumbotron</h1>
-                                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                            </div>
-                        </div>
-                    </div>
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
+		<title>KadoLeen</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-md navbar-fixed-top navbar-light" style="background-color: #cc00ff;">
+			<div class="navbar-collapse collapse nav-content order-2 order-md-4">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="#" class="pull-left"><img src="favicon.ico"></a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+					</li>
+					<li class="nav-item active">
+							<a class="nav-link pl-0" href="/">adoLeen</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="/">Home</a>
+					</li>
+				</ul>
+			</div>
+			<ul class="nav navbar-nav text-nowrap flex-row mx-md-auto order-1 order-md-4">
+				<form class="form-inline my-2 my-lg-0">
+					<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				</form>
+			</ul>
+			<div class="ml-auto navbar-collapse collapse nav-content order-3 order-md-4">
+				<ul class="ml-auto nav navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="/contact">Contact</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/about">About</a>
+					</li>
+					<li class="nav-item active dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">My Account</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+							<a class="dropdown-item" href="#">Profile</a>
+							<a class="dropdown-item" href="#">Cart</a>
+							<a class="dropdown-item" href="#">Logout</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="jumbotron jumbotron-fluid">
+						<div class="container">
+					<h1 class="display-4">Fluid jumbotron</h1>
+					<p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
 </html>
